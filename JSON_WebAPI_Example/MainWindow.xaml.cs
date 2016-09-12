866a18b6b1a7d0df089481445f168dd652a65077
s00141500 <s00141500@mail.itsligo.ie>
2016-09-12 02:42:09 +0100

    added files to repo

---
+++ JSON_WebAPI_Example/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using JSON_WebAPI_Example;
+using System.Web.Script.Serialization; // adds this but before add rference --> System.Web.Extensions
+using Newtonsoft.Json; // adds this but before add rference --> JSON.net
+
+namespace JSON_WebAPI_Example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // fetch data as Json String
+            var url = new Uri("http://localhost:1234/MyService.svc/json/4");
+            var client = new System.Net.WebClient();
+            // var json =  await client.DownloadStringTaskAsync(url);
+            var json = client.DownloadStringTaskAsync(url);
+
+            // deserialize JSON into object
+            var serializer = new JavaScriptSerializer();
+
+            //var jsonResult =  serializer.Deserialize(json);
+
+            // use the objects 
+        //    tblkJSON.Text = data;
+        }
+    }
+}
---
+++ WcfService1/IService1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate="/json/{number}",ResponseFormat= WebMessageFormat.Json)]
+        // TODO: Add your service operations here
+         public Data GetJSON(string number)
+        {
+            var x = new Data(int.Parse(number));
+            return new Data(int.Parse(number));
+        }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    }
+
+    public class Data
+    {
+        public int[] Multiples { get; set; }
+
+        public Data(int num)
+        {
+            Multiples = new int[10];
+
+            for (int i = 0; i < Multiples.Length; i++)
+            {
+                Multiples[i] = i;
+            }
+        }
+    }
+}
